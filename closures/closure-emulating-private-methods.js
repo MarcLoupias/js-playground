a5bf297341a51965a3c05b1ef7da5e6d054c8b38
marlou <pro@marc-loupias.fr>
2017-08-14 17:26:50 +0200

    closures - emulating private methods

---
+++ closures/closure-emulating-private-methods.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+'use strict';
+
+const assert = require('assert');
+
+// using IIFE ...
+
+var counter = (function() {
+    var privateCounter = 0;
+    function changeBy(val) {
+        privateCounter += val;
+    }
+    return {
+        increment: function() {
+            changeBy(1);
+        },
+        decrement: function() {
+            changeBy(-1);
+        },
+        value: function() {
+            return privateCounter;
+        }
+    };
+})();
+
+// ... counter var can be used immediatly
+
+assert.equal(counter.value(), 0);
+
+counter.increment();
+counter.increment();
+assert.equal(counter.value(), 2);
+
+counter.decrement();
+assert.equal(counter.value(), 1);
+
+// using a factory function ...
+
+var makeCounter = function() {
+    var privateCounter = 0;
+    function changeBy(val) {
+        privateCounter += val;
+    }
+    return {
+        increment: function() {
+            changeBy(1);
+        },
+        decrement: function() {
+            changeBy(-1);
+        },
+        value: function() {
+            return privateCounter;
+        }
+    }
+};
+
+// ... we can build how many counter as we want to
+
+var counter1 = makeCounter();
+var counter2 = makeCounter();
+assert.equal(counter1.value(), 0);
+assert.equal(counter2.value(), 0);
+
+counter1.increment();
+counter1.increment();
+assert.equal(counter1.value(), 2);
+assert.equal(counter2.value(), 0);
+
+counter1.decrement();
+assert.equal(counter1.value(), 1);
+assert.equal(counter2.value(), 0);
