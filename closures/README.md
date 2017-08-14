d65d6595a0dcf15fe945dafd360840ed949dc8d7
marlou <pro@marc-loupias.fr>
2017-08-14 16:46:36 +0200

    basic closure example

---
+++ closures/README.md
@@ -0,0 +1,3 @@
+# closures
+
+Executables examples from my [memo-dev gitbook](https://marcloupias.gitbooks.io/memo-dev/content/languages/javascript/js-closures.html).
---
+++ closures/closure-basic-example.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+'use strict';
+
+const assert = require('assert');
+
+function counter() {
+    let count = 0;
+    return {
+        getCount: function getCount() {
+            return count;
+        },
+        increment: function increment() {
+            count += 1;
+        }
+    };
+}
+
+const counterInstance = counter();
+
+console.log('closure-basic-example - count var is initialized to 0');
+assert.equal(counterInstance.getCount(), 0, 'count var in counter function lexical scope is equal to 0');
+
+counterInstance.increment();
+assert.equal(counterInstance.getCount(), 1, 'count var in counter function lexical scope is equal to 1');
+
+counterInstance.increment();
+counterInstance.increment();
+assert.equal(counterInstance.getCount(), 3, 'count var in counter function lexical scope is equal to 3');
+
+let count = counterInstance.getCount();
+
+console.log(`closure-basic-example - counter is ${count}`);
