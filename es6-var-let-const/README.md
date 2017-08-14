a096f9536cd79d9712c0ee6abaa13a2d19104ba9
marlou <pro@marc-loupias.fr>
2017-08-14 17:14:42 +0200

    es6 var, let and const differences

---
+++ es6-var-let-const/README.md
@@ -0,0 +1,6 @@
+# ES6 : var, let or const ?
+
+See [JavaScript ES6+: var, let, or const?](https://medium.com/javascript-scene/javascript-es6-var-let-or-const-ba58b8dcde75) Eric Elliott article.
+
+> Perhaps the most important thing you can learn to be a better coder is to **keep things simple**. 
+> In the context of identifiers, that means that **a single identifier should only be used to represent a single concept**.
---
+++ es6-var-let-const/var-let-const-example.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+'use strict';
+
+const assert = require('assert');
+
+/*
+ * `const` means that the identifier can’t be reassigned
+ */
+
+const person = {name: 'Robert'};
+//person = {name: 'Marcel'}; // throws `TypeError: Assignment to constant variable.`
+
+// `const` have nothing to do with immutability, the variable value is not constant, only the variable reference
+assert.equal(person.name, 'Robert');
+person.name = 'Marcel';
+assert.equal(person.name, 'Marcel', 'const person is mutable');
+
+/*
+ *  `let`, is a signal :
+ */
+
+// - that the variable may be reassigned, such as a counter in a loop, or a value swap in an algorithm
+let counterObject = {count: 0};
+assert.equal(counterObject.count, 0);
+counterObject = {count: 1};
+assert.equal(counterObject.count, 1);
+
+// - that the variable will be used only in the block it’s defined in, which is not always the entire containing function.
+function testLetBlockedScope () {
+    let counterObject;
+    assert.equal(counterObject, undefined);
+    counterObject = {count: 111};
+    assert.equal(counterObject.count, 111);
+}
+
+testLetBlockedScope();
+assert.equal(counterObject.count, 1);
+
+/*
+ * `var`
+ *
+ * is now the weakest signal available when you define a variable in JavaScript.
+ * The variable may or may not be reassigned,
+ * and may or may not be used for an entire function, or just for the purpose of a block or loop.
+ */
