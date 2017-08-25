43229a414c11c164e79d558b23a47cce781c55cb
marlou <pro@marc-loupias.fr>
2017-08-26 00:43:03 +0200

    this and bind basic example

---
+++ this-and-bind/README.md
@@ -0,0 +1,6 @@
+# `this` and `bind`
+
+[bind and this - Object Creation in JavaScript P1 - FunFunFunction](https://www.youtube.com/watch?v=GhbhD1HR5vk&index=1&list=PL0zVEGEvSaeHBZFy6Q8731rcwk0Gtuxub)
+
+There is no *right* way to create object in javascript.
+
---
+++ this-and-bind/this-and-bind-example.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+'use strict';
+
+const assert = require('assert');
+
+const dog = {
+    sound: 'woof',
+    talk: function() {
+        return this.sound;
+    }
+};
+
+assert.equal(dog.talk(), 'woof');
+
+const talkFn = dog.talk; // we can assign functions to variables, functions are values
+
+assert.throws(talkFn, function(err) {
+    return !!(err instanceof Error && err.message === "Cannot read property 'sound' of undefined");
+}, 'this is undefined, accessing it throws an error');
+
+/*
+ it is an example of the object oriented influence over javascript clashes with the functionnal
+ programming influence.
+
+ JavaScript is influenced by Java and Scheme where the first is OOP language and the second is FP
+ language.
+
+ In FP there is no this concept because this means shared state and shared state is evil in FP.
+ */
+
+const boundFn = talkFn.bind(dog);
+
+// bind forces the context to dog, so this is defined.
+assert.equal(boundFn(), 'woof');
