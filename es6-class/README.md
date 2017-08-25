47679c9cb666d656ff7715f938b62be9972a83e1
marlou <pro@marc-loupias.fr>
2017-08-25 23:43:53 +0200

    class - basics

---
+++ es6-class/README.md
@@ -0,0 +1,3 @@
+# ES6 : class
+
+[Class keyword - Object Creation in JavaScript P7 - Fun Fun Function](https://www.youtube.com/watch?v=Tllw4EPhLiQ)
---
+++ es6-class/class-example.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+'use strict';
+
+const assert = require('assert');
+
+/*
+ * empty form
+ * */
+
+class Mammal {
+
+}
+
+let fluffykins = new Mammal();
+assert.deepEqual(fluffykins, {}, 'fluffykins should be an empty object like a litteral empty object');
+
+/*
+ * constructor
+ * */
+
+class Mammal2 {
+    constructor(sound) {
+        this.sound = sound;
+    }
+}
+
+let fluffykins2 = new Mammal2('woof!');
+assert.deepEqual(fluffykins2, {sound: 'woof!'}, 'fluffykins should be an object like a litteral object');
+
+/*
+ * methods
+ * */
+
+class Mammal3 {
+    constructor(sound) {
+        this.sound = sound;
+    }
+
+    talk() {
+        return this.sound;
+    }
+}
+
+let fluffykins3 = new Mammal3('woof!');
+assert.deepEqual(fluffykins3.talk(), 'woof!');
+
+/*
+ * inheritance
+ * */
+
+class Dog extends Mammal3 {
+    constructor() {
+        super('dog woof!');
+    }
+}
+
+let fluffykins3Dog = new Dog();
+assert.deepEqual(fluffykins3Dog.talk(), 'dog woof!');
+
+/*
+ * classes in ES6 are not really classes ...
+ * */
+
+assert.ok('function' === typeof Dog, 'a class is a function');
+assert.ok(Dog.prototype.isPrototypeOf(fluffykins3Dog) === true, 'class inheritance is built on top of prototype inheritance');
