bcad43ca1b0a1f36d7a45a7078c30b91d8f4bcee
marlou <pro@marc-loupias.fr>
2018-09-16 14:24:41 +0200

    typescript type assignation

--- typescript/ts-hello-world/greeter.ts
+++ typescript/ts-hello-world/greeter.ts
@@ -1,7 +1,10 @@
-function greeter(person) {
+function greeter(person: string) {
     return `Hello, ${person}`;
 }
 
 const user = "Marco User";
+
+// to test : error TS2345: Argument of type 'number[]' is not assignable to parameter of type 'string'
+//const user = "Marco User";
 
 console.log(greeter(user));
