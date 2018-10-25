c0dfe5b5805411451b67c31ca4261f5611e08fb8
marlou <pro@marc-loupias.fr>
2018-10-25 20:56:21 +0200

    handling-cli - sync testing approach refacto

--- handling-cli/sync-test.js
+++ handling-cli/sync-test.js
@@ -1,38 +1,66 @@
 'use strict';
 
 const expect = require('chai').expect;
+const assert = require('assert');
 const spawnSync = require('child_process').spawnSync;
 
-describe('Testing -v option', function () {
-    // If your CLI tool is taking some expected time to start up / tear down, you
-    // might want to set this to avoid slowness warnings.
-    this.slow(600);
+function executeCliSync(args) {
+    assert.ok(Array.isArray(args));
 
-    it('should return the package version number', () => {
-        // the js file MUST have 777 access
-        const result = spawnSync(
-            './commander-basic.js',
-            ['-v'],
-            { encoding: 'utf-8' }
-        );
+    return spawnSync(
+        './index.js',
+        args,
+        { encoding: 'utf-8' }
+    );
+}
 
-        expect(result.stderr).to.be.empty;
-        expect(result.status).to.equal(0);
-        console.log('result = ', result);
-        expect(result.stdout).to.include('1.0.0');
+describe('Testing CLI with :', function () {
+    context('-v', function() {
+        let result;
+
+        beforeEach(function() {
+            result = executeCliSync(['-v']);
+        });
+
+        it('should exit with 0 status', () => {
+            expect(result.status).to.equal(0);
+        });
+
+        it('should return the package version number', () => {
+            expect(result.stdout).to.equal('1.0.0\n');
+        });
     });
 
-    it('should return a pizza with peppers and toto cheese', () => {
-        // the js file MUST have 777 access
-        const result = spawnSync(
-            './commander-basic.js',
-            ['-p', '-c', 'toto'],
-            { encoding: 'utf-8' }
-        );
+    context('--version', function() {
+        let result;
 
-        expect(result.stderr).to.be.empty;
-        expect(result.status).to.equal(0);
-        console.log('result = ', result);
-        expect(result.stdout).to.equal('you ordered a pizza with:\n  - peppers\n  - toto cheese\n');
+        beforeEach(function() {
+            result = executeCliSync(['--version']);
+        });
+
+        it('should exit with 0 status', () => {
+            expect(result.status).to.equal(0);
+        });
+
+        it('should return the package version number', () => {
+            expect(result.stdout).to.equal('1.0.0\n');
+        });
+    });
+
+    context('-p -c toto', function() {
+        let result;
+
+        beforeEach(function() {
+            result = executeCliSync(['-p', '-c', 'toto']);
+        });
+
+        it('should exit with 0 status', () => {
+            expect(result.status).to.equal(0);
+        });
+
+        it('should return a pizza with peppers and toto cheese', () => {
+            expect(result.stdout).to.equal('you ordered a pizza with:\n  - peppers\n  - toto cheese\n');
+        });
     });
 });
+
